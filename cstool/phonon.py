b9261a2756dc65d3fbad3a14bae67adf08f31676
Johan Hidding <j.hidding@esciencecenter.nl>
2017-02-21 15:40:38 +0100

    phonon.py is lint-free

--- cstool/phonon.py
+++ cstool/phonon.py
@@ -24,11 +24,12 @@ def phonon_crosssection(eps_ac, c_s, M, rho_m,
     where the unit must have the same dimensionality as those given here.
 
     :param eps_ac: acoustic deformation potential (eV)
-    :param c_s: speed of sound (m/s) unit? 
+    :param c_s: speed of sound (m/s) unit?
     :param M: molar weight (g/mol)
     :param rho_m: mass density (g/cm³)
     :param lattice: lattice constant (Å)
-    :param E_BZ: the electron energy at the Brioullin zone (eV); can be deduced from `lattice`.
+    :param E_BZ: the electron energy at the Brioullin zone (eV); can be deduced
+    from `lattice`.
     :return: Function taking an energy array and an angle array, returning the
              crosssection quantity in units of cm² as a 2d-array.
 
@@ -36,35 +37,51 @@ def phonon_crosssection(eps_ac, c_s, M, rho_m,
     """
 
     # Material related parameters. These will be moved to argument
-    alpha_single_branch = 0. * units('m²/s')  # relates to the bending of the dispersion relation towards the Brillouin zone boundary (used in Eq. 3.112)
-    m_dos = 1 * units.m_e               # :param m_dos: density of state mass (kg)
-    m_effective = 1 * units.m_e         # :param m_effective: effective mass of particle a.k.a. m_star (kg)
+    # relates to the bending of the dispersion relation towards the Brillouin
+    # zone boundary (used in Eq. 3.112)
+    alpha_single_branch = 0. * units('m²/s')
+    # TODO: make parameter. :param m_dos: density of state mass (kg)
+    m_dos = 1 * units.m_e
+    # TODO: make parameter. :param m_effective: effective mass of particle
+    # a.k.a. m_star (kg)
+    m_effective = 1 * units.m_e
 
     if lattice is None and E_BZ is None:
         raise ValueError("One of `lattice` and `E_BZ` should be given.")
 
-    k_BZ = 2 * pi / lattice # wave factor at 1st Brillouin Zone Boundary
-        
-    E_BZ = E_BZ or ((units.hbar * k_BZ)**2 / (2 * units.m_e)).to('eV')  # Verduin Eq. 3.120
-    lattice = lattice or np.sqrt(units.h**2 / (2 * units.m_e*E_BZ)).to('Å') # If lattice is not given, but E_BZ is defined.
+    # wave factor at 1st Brillouin Zone Boundary
+    k_BZ = 2 * pi / lattice
+
+    # Verduin Eq. 3.120
+    E_BZ = E_BZ or ((units.hbar * k_BZ)**2 / (2 * units.m_e)).to('eV')
+
+    # If lattice is not given, but E_BZ is defined.
+    lattice = lattice or np.sqrt(units.h**2 / (2 * units.m_e*E_BZ)).to('Å')
 
     # print("E_BZ = {:~P}".format(E_BZ.to('eV'))) ??
 
-    A = 5*E_BZ                                  # A: screening factor (eV); 5 is constant for every material.
-    rho_n = (units.N_A / M * rho_m).to('cm⁻³')  # rho_n: number density.
-    
-    h_bar_w_BZ = (units.hbar * c_s *k_BZ - units.hbar * alpha_single_branch * k_BZ**2).to('eV') # Verduin Eq. 3.114
-    n_BZ = 1 / (expm1(h_bar_w_BZ / (units.k * T)) - 1) # Acoustic phonon population density , Verduin Eq. 3.117
-    
+    # A: screening factor (eV); 5 is constant for every material.
+    A = 5*E_BZ
+    # rho_n: number density.
+    rho_n = (units.N_A / M * rho_m).to('cm⁻³')
+
+    h_bar_w_BZ = (units.hbar * (c_s*k_BZ - alpha_single_branch * k_BZ**2)) \
+        .to('eV')    # Verduin Eq. 3.114
+
+    # Acoustic phonon population density , Verduin Eq. 3.117
+    n_BZ = 1 / (expm1(h_bar_w_BZ / (units.k * T)) - 1)
+
+    # Verduin equation (3.125) divided by number density
     sigma_ac = ((np.sqrt(m_effective * m_dos**3) * eps_ac**2 * units.k * T) /
-                (pi * units.hbar**4 * c_s**2 * rho_m * rho_n)).to('cm²') # Verduin equation (3.125) divided by number density
-    # 
-    # extra multiplication factor for high energies according to Verduin equation (3.126)
-    # noticed that A could be balanced out of the equation
-    factor_high = ((n_BZ + 0.5) * 8 * m_dos * c_s**2 / (h_bar_w_BZ * units.k * T)).to('1/eV')
+                (pi * units.hbar**4 * c_s**2 * rho_m * rho_n)).to('cm²')
+
+    # extra multiplication factor for high energies according to Verduin
+    # equation (3.126) noticed that A could be balanced out of the equation
+    factor_high = ((n_BZ + 0.5) * 8 * m_dos * c_s**2 /
+                   (h_bar_w_BZ * units.k * T)).to('1/eV')
     # alpha = ((n_BZ + 0.5) * 8 * h_bar_w_BZ / (units.k*T * E_BZ)).to('1/eV')
 
-    def mu(theta):  # see Eq. 3.126  
+    def mu(theta):  # see Eq. 3.126
         return (1 - cos(theta)) / 2
 
     def norm(mu, E):
@@ -97,7 +114,7 @@ def phonon_crosssection(eps_ac, c_s, M, rho_m,
 def phonon_cs_fn(s: Settings):
     return phonon_crosssection(
         s.eps_ac, s.c_s, s.M_tot, s.rho_m, s.lattice,
-        interpolate=log_interpolate) # , h=lambda x: x)
+        interpolate=log_interpolate)  # , h=lambda x: x)
 
 
 if __name__ == "__main__":
