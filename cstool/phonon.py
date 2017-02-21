9f0656bb6bacb7e3cbf2c603eb4d152e6e817d67
Kerim Arat <k.t.arat@tudelft.nl>
2017-02-21 12:04:50 +0100

    Fix errors in phonon Xsections

--- cstool/phonon.py
+++ cstool/phonon.py
@@ -18,36 +18,53 @@ def phonon_crosssection(eps_ac, c_s, M, rho_m,
                         lattice=None, E_BZ=None, T=units.T_room,
                         interpolate=log_interpolate,
                         h=lambda x: (3 - 2*x) * x**2):
-    """Compute the differential phonon-crosssections given the properties
+    """
+    Compute the differential phonon cross-sections given the properties
     of a material. These properties should be given as quantities with units,
     where the unit must have the same dimensionality as those given here.
 
-    :param eps_ac: accoustic deformation potential (eV)
-    :param c_s: speed of sound (km/s)
+    :param eps_ac: acoustic deformation potential (eV)
+    :param c_s: speed of sound (km/s) unit? 
     :param M: molar weight (g/mol)
     :param rho_m: mass density (g/cm³)
     :param lattice: lattice constant (Å)
-    :param E_BZ: Brioullin zone energy (eV), can be deduced from `lattice`.
+    :param E_BZ: the electron energy at the Brioullin zone (eV); can be deduced from `lattice`.
     :return: Function taking an energy array and an angle array, returning the
-        crosssection quantity in units of cm² as a 2d-array.
+             crosssection quantity in units of cm² as a 2d-array.
 
     One of the parameters `lattice` and `E_BZ` should be given.
     """
+
+    # Material related parameters. These will be moved to argument
+    alpha_single_branch = 0         # relates to the bending of the dispersion relation towards the Brillouin zone boundary (used in Eq. 3.112)
+    m_dos = units.m_e               # :param m_dos: density of state mass (kg)
+    m_effective = units.m_e         # :param m_effective: effective mass of particle a.k.a. m_star (kg)
+
     if lattice is None and E_BZ is None:
         raise ValueError("One of `lattice` and `E_BZ` should be given.")
 
-    E_BZ = E_BZ or (units.h**2 / (2*units.m_e * lattice**2)).to('eV')
+    k_BZ = 2 * pi / lattice # wave factor at 1st Brillouin Zone Boundary
+        
+    E_BZ = E_BZ or ( (units.h * k_BZ)**2 / (2*units.m_e) ).to('eV')  # Is that correct ? (Eq. 3.120)
+    lattice = lattice or sqrt(units.h**2/(2*units.m_e*E_BZ)).to('Å') # If lattice is not given, but E_BZ is defined.
 
-    # print("E_BZ = {:~P}".format(E_BZ.to('eV')))
+    # print("E_BZ = {:~P}".format(E_BZ.to('eV'))) ??
 
-    A = 5*E_BZ
-    rho_n = (units.N_A / M * rho_m).to('cm⁻³')
-    h_bar_w_BZ = units.h * c_s / lattice
-    n_BZ = 1 / expm1(h_bar_w_BZ / (units.k * T))
-    sigma_ac = ((units.m_e**2 * eps_ac**2 * units.k * T) /
-                (units.hbar**4 * c_s**2 * rho_m * rho_n)).to('cm²')
+    A = 5*E_BZ                                  # A: screening factor (eV); 5 is constant for every material.
+    rho_n = (units.N_A / M * rho_m).to('cm⁻³')  # rho_n: number density.
+    
 
-    alpha = ((n_BZ + 0.5) * 4 * h_bar_w_BZ / (units.k*T * E_BZ)).to('1/eV')
+    w_AC = c_s*
+    h_bar_w_BZ = units.h * c_s / lattice - units.h * alpha_single_branch * 2 * pi / lattice**2
+    n_BZ = 1 / (expm1(h_bar_w_BZ / (units.k * T)) - 1)              # Acoustic phonon population density ()
+    
+    sigma_ac = ((sqrt(m_effective * m_density_of_states**3) * eps_ac**2 * units.k * T) /
+                (pi * units.hbar**4 * c_s**2 * rho_m * rho_n)).to('cm²') # equation (3.125) divided by number density
+
+    # extra multiplication factor for high energies according to equation (3.126)
+    # noticed that A could be balanced out of the equation
+    factor_high = ((n_BZ + 0.5) * 8 * m_density_of_states * c_s**2 / (h_bar_w_BZ * units.k * T)).to('1/eV')
+    #alpha = ((n_BZ + 0.5) * 8 * h_bar_w_BZ / (units.k*T * E_BZ)).to('1/eV')
 
     def mu(theta):
         return (1 - cos(theta)) / 2
@@ -60,7 +77,7 @@ def phonon_crosssection(eps_ac, c_s, M, rho_m,
 
         :param E: energy in Joules.
         :param theta: angle in radians."""
-        return (alpha * mu * E).to(units.dimensionless)
+        return (factor_high * mu * E).to(units.dimensionless)
 
     def dcs(E, theta):
         m = mu(theta)
@@ -78,7 +95,7 @@ def phonon_crosssection(eps_ac, c_s, M, rho_m,
 def phonon_cs_fn(s: Settings):
     return phonon_crosssection(
         s.eps_ac, s.c_s, s.M_tot, s.rho_m, s.lattice,
-        interpolate=log_interpolate)  # , h=lambda x: x)
+        interpolate=log_interpolate) # , h=lambda x: x)
 
 
 if __name__ == "__main__":
