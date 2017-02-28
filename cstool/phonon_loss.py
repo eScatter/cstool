752c8b49a40b873353502b891d47efd5e5a9d823
Kerim Arat <k.t.arat@tudelft.nl>
2017-02-28 16:41:23 +0100

    fix phonon single branch & phonon_loss

--- cstool/phonon_loss.py
+++ cstool/phonon_loss.py
@@ -4,40 +4,83 @@ from scipy.integrate import quad
 import numpy as np
 
 
-def phonon_loss(c_s, a, T):
-    """Compute the phonon loss.
+def branch_loss(c_s, alpha, lattice, T):
+    """Compute the net average energy loss of phonons.
 
-    :param c_s:
-        Speed of sound.
-    :type c_s: [velocity]
-
-    :param a:
-        Lattice spacing
-    :type a: [length]
-
-    :param T:
-        Temperature
-    :type T: [temperature]
+    :param c_s_lo: speed of sound for longitudinal mode (m/s)
+    :param alpha_lo: relates to the bending of the dispersion relation
+    towards the Brillouin zone boundary for longitudinal mode (m²/s)
+    :param lattice: Lattice spacing (A)
+    :param T: Temperature (K)
     """
-    # compute everything in SI units
-    c_s = c_s.to('m/s').magnitude
-    a = a.to('m').magnitude
-    kT = (1 * units.k * T).to('J').magnitude
-    hbar = (1 * units.hbar).to('J s').magnitude
 
-    def w(k):
-        return 2*c_s / a*np.sin(k * a/2)
+    kT = (1 * units.k * T).to('J')  # 'k' is Boltzmann constant
 
-    def N(k):
-        return 1. / np.expm1(hbar * w(k) / kT)
+    # Wave factor at 1st Brillouin Zone Boundary
+    k_BZ = 2 * pi / lattice
 
-    def x1(k):
-        return w(k)*k*k
+    # Calculate average net loss per acoustic scattering event
 
-    def x2(k):
-        return (2*N(k) + 1) * k*k
+    # Isotropic Dispersion Relation, Verduin (Eq. 3.112)
+    def h_bar_w_AC(k, c_s, alpha):
+        return units.hbar * (c_s * k - alpha * k**2).to('1/s')  # Verduin Eq. 3.114
 
-    y1, err1 = quad(x1, 0, np.pi/a)
-    y2, err2 = quad(x2, 0, np.pi/a)
+    # Bose-Einstein distribution, Verduin (Eq. 3.117)
+    def N_BE(k, c_s, alpha):
+        return 1. / np.expm1(h_bar_w_AC(k, c_s, alpha) / kT)
 
-    return hbar * y1/y2 * units.J
+    # (Verduin Eq. 3.116)
+    def nominator(k, c_s, alpha):
+        return  h_bar_w_AC(k, c_s, alpha) * k**2
+
+    def denominator(k, c_s, alpha):
+        return (2 * N_BE(k, c_s, alpha) + 1) * k**2
+
+    # TO DO: strip the units https://pint.readthedocs.io/en/0.7.2/wrapping.html
+    y1, err1 = quad(nominator, 0, k_BZ, args=(c_s, alpha,))
+    y2, err2 = quad(denominator, 0, k_BZ, args=(c_s, alpha,))
+    # TO DO: assign the units back
+
+    return  (y1 / y2).to('J')
+
+
+def phonon_loss(s: Settings):
+    float energy_loss = 0
+    if s.phonon.model == 'dual':
+        # Calculate net average energy loss of multiple branches:
+        # 1*Longitidunal + 2*Transversal Branch
+        energy_loss = (1.0 * branch_loss(s.phonon.longitudinal.c_s,
+                                         s.phonon.longitudinal.alpha,
+                                         s.phonon.lattice,
+                                         T=units.T_room) +
+                       2.0 * branch_loss(s.phonon.transversal.c_s,
+                                         s.phonon.transversal.alpha,
+                                         s.phonon.lattice,
+                                         T=units.T_room)) / 3
+        return energy_loss
+    else:
+        # Calculate net average energy loss of single branch:
+        # 1*Longitidunal branch
+        energy_loss = 1.0 * branch_loss(s.phonon.single.c_s,
+                                        s.phonon.single.alpha,
+                                        s.phonon.lattice,
+                                        T=units.T_room)
+        return energy_loss
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description='Calculate energy loss for AC phonons for a material.')
+    parser.add_argument(
+        'material_file', type=str,
+        help="Filename of material in JSON format.")
+    args = parser.parse_args()
+
+    s = read_input(args.material_file)
+
+    # def phonon_loss(c_s, lattice, T):
+    float energy_loss = phonon_loss(s)
+    print("Phonon energy loss:" + str(energy_loss) + "eV for")
+    print "For" s.phonon.model, "branch"
