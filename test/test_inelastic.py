01d5d2badeeb83b1ddcc1fcbff8f5220098bb72b
Luc van Kessel <L.C.P.M.vanKessel@tudelft.nl>
2017-06-07 15:18:08 +0200

    Fix tests.
    A recent commit changed the model format. Turns out that the tests had their data hard-coded, so they broke. They now read their data from file.

--- test/test_inelastic.py
+++ test/test_inelastic.py
@@ -1,45 +1,13 @@
-from cstool.parse_input import (parse_to_model, check_settings, cstool_model)
+from cstool.parse_input import (read_input, check_settings, cstool_model)
 from cstool.inelastic import (inelastic_cs_fn)
 from cslib import (units)
 
 import numpy as np
 
-pmma = {
-    "name": "pmma",
-    "rho_m": "1.192 g/cm³",
-    "fermi": "0 eV",
-    "work_func": "2.5 eV",
-
-    "phonon": {
-        "model": "dual",
-        "lattice": "5.43 Å",
-        "single": {
-            "alpha": "2.13e-7 m²/s",
-            "c_s": "2750 m/s",
-            "eps_ac": "9.2 eV"},
-        "longitudinal": {
-            "alpha": "2.00e-7 m²/s",
-            "c_s": "2750 m/s",
-            "eps_ac": "9.2 eV"},
-        "transversal": {
-            "alpha": "2.26e-7 m²/s",
-            "c_s": "2750 m/s",
-            "eps_ac": "9.2 eV"}
-        },
-
-    "band_gap": "5.6 eV",
-    "elf_file": "data/elf/df_PMMA.dat",
-    "elements": {
-        "H":  {"count": 8, "Z": 1, "M":  "1.008 g/mol"},
-        "C":  {"count": 5, "Z": 6, "M": "12.011 g/mol"},
-        "O":  {"count": 2, "Z": 8, "M": "15.999 g/mol"}}
-}
-
-
 def test_inelastic_cs_fn():
     """Tests that the inelastic subroutine returns a function that
     can handle arrays and returns correct units."""
-    settings = parse_to_model(cstool_model, pmma)
+    settings = read_input('data/materials/pmma.yaml')
     if not check_settings(settings, cstool_model):
         raise ValueError("Parsed settings do not conform the model.")
 
