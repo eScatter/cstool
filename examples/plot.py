d88887d073fe45f8aabb03fed06d8e7307f8012d
Sebastiaan Lokhorst <sebastiaanlokhorst@gmail.com>
2017-07-14 17:40:37 +0200

    Plot ionization probabilities instead of total cross-section

--- examples/plot.py
+++ examples/plot.py
@@ -1,6 +1,8 @@
 import numpy as np
 import h5py as h5
 import matplotlib.pyplot as plt
+from collections import defaultdict
+import math
 
 from cslib import units
 
@@ -9,9 +11,11 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(
         description='Plot cross-sections from HDF5 material file.')
-    parser.add_argument(
-        'material_file', type=str,
+    parser.add_argument('material_file', type=str,
         help="Filename of material in HDF5 format.")
+    parser.add_argument('--elastic', action='store_true')
+    parser.add_argument('--inelastic', action='store_true')
+    parser.add_argument('--ionization', action='store_true')
     args = parser.parse_args()
 
     infile = h5.File(args.material_file, 'r')
@@ -28,43 +32,61 @@ if __name__ == "__main__":
         print('{: <16} {}'.format(name, value))
     print()
 
+    if not (args.elastic or args.inelastic or args.ionization):
+        print('Not plotting anything. Use the -h flag for usage.')
+
     def dataset_units(dataset):
         return np.array(dataset) * units(dataset.attrs['units'])
 
-    # elastic
-    el_energy_dat = dataset_units(infile['elastic/energy'])
-    el_cs_dat = dataset_units(infile['elastic/cross_section'])
-    el_icdf_dat = dataset_units(infile['elastic/angle_icdf'])
-    print('plotting elastic cross-section...')
-    plt.loglog(el_energy_dat.to('eV'), el_cs_dat.to('nm^2'))
-    plt.title('elastic cross-section')
-    plt.xlabel('$K$ [eV]')
-    plt.ylabel('$\sigma$ [m²]')
-    plt.show()
-    print()
+    if args.elastic:
+        print('loading elastic data...')
+        el_energy_dat = dataset_units(infile['elastic/energy'])
+        el_cs_dat = dataset_units(infile['elastic/cross_section'])
+        el_icdf_dat = dataset_units(infile['elastic/angle_icdf'])
 
-    # inelastic
-    inel_energy_dat = dataset_units(infile['inelastic/energy'])
-    inel_cs_dat = dataset_units(infile['inelastic/cross_section'])
-    inel_icdf_dat = dataset_units(infile['inelastic/w0_icdf'])
-    print('plotting inelastic cross-section...')
-    plt.loglog(inel_energy_dat.to('eV'), inel_cs_dat.to('nm^2'))
-    plt.title('elastic cross-section')
-    plt.xlabel('$K$ [eV]')
-    plt.ylabel('$\sigma$ [m²]')
-    plt.show()
-    print()
+        plt.loglog(el_energy_dat.to('eV'), el_cs_dat.to('nm^2'))
+        plt.title('elastic cross-section')
+        plt.xlabel('$K$ [eV]')
+        plt.ylabel('$\sigma$ [m²]')
+        plt.show()
 
-    # ionization
-    ion_energy_dat = dataset_units(infile['ionization/energy'])
-    ion_cs_dat = dataset_units(infile['ionization/cross_section'])
-    ion_icdf_dat = dataset_units(infile['ionization/dE_icdf'])
-    print('plotting ionization cross-section...')
-    plt.loglog(ion_energy_dat.to('eV'), ion_cs_dat.to('nm^2'))
-    plt.title('ionization cross-section')
-    plt.xlabel('$K$ [eV]')
-    plt.ylabel('$\sigma$ [m²]')
-    plt.show()
-    print()
+    if args.inelastic:
+        print('loading inelastic data...')
+        inel_energy_dat = dataset_units(infile['inelastic/energy'])
+        inel_cs_dat = dataset_units(infile['inelastic/cross_section'])
+        inel_icdf_dat = dataset_units(infile['inelastic/w0_icdf'])
+
+        plt.loglog(inel_energy_dat.to('eV'), inel_cs_dat.to('nm^2'))
+        plt.title('elastic cross-section')
+        plt.xlabel('$K$ [eV]')
+        plt.ylabel('$\sigma$ [m²]')
+        plt.show()
+
+    if args.ionization:
+        print('loading ionization data...')
+        ion_energy_dat = dataset_units(infile['ionization/energy'])
+        ion_icdf_dat = dataset_units(infile['ionization/dE_icdf'])
+
+        shells = {}
+        for i, K in enumerate(ion_energy_dat):
+            shell_p = defaultdict(int)
+            for B in ion_icdf_dat[i, :]:
+                if not math.isnan(B.magnitude):
+                    shell_p[B.magnitude] += 1
+            for B, P in shell_p.items():
+                if B not in shells:
+                    shells[B] = {}
+                shells[B][K.magnitude] = P/ion_icdf_dat.shape[1]
+
+        legends = []
+        for B, K_P in shells.items():
+            K, P = zip(*K_P.items())
+            legends.append(B)
+            plt.semilogx(K, P)
+        plt.legend(legends)
+        plt.title('ionization energy probability')
+        plt.xlabel('$K$ [eV]')
+        plt.ylabel('P')
+        plt.show()
 
     infile.close()
